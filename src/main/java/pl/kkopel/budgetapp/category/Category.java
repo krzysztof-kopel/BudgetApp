c864ab142d55eb7579102ad32ba81fa73d29c8b0
krzysbielsko <krzysbielsko@gmail.com>
2026-06-08 13:51:49 +0200

    Account and Category entities.

---
+++ src/main/java/pl/kkopel/budgetapp/category/Category.java
@@ -0,0 +1,60 @@
+package pl.kkopel.budgetapp.category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name="\"Categories\"")
+public class Category {
+    @Id
+    @Column(name="id")
+    private UUID id;
+
+    @Column(name="name", nullable=false, unique=true)
+    private String name;
+
+    @Column(name="budget_limit")
+    private BigDecimal budget_limit;
+
+    public Category() {
+        this.id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBudget_limit() {
+        return budget_limit;
+    }
+
+    public void setBudget_limit(BigDecimal budget_limit) {
+        this.budget_limit = budget_limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", budget_limit=" + budget_limit +
+                '}';
+    }
+}
