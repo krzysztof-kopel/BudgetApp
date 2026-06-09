c1c47b454534bfcb4dea1307d3cd74555feff7e3
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2026-06-09 21:17:06 +0200

    Example categories.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 insert into "TransactionTypes" ("id") values ('INCOME') on conflict do nothing;
 insert into "TransactionTypes" ("id") values ('EXPENSE') on conflict do nothing;
+insert into "Categories" ("id", "name", "budget_limit") values ('5123686d-0e89-4d1b-b162-ece1dc742413', 'Groceries', '2000') on conflict do nothing;
+insert into "Categories" ("id", "name", "budget_limit") values ('98763ccb-bd76-404a-babb-bcf0f57c9254', 'Car', '5000') on conflict do nothing;
+insert into "Categories" ("id", "name", "budget_limit") values ('1cee9612-6911-45d4-a1b5-1c651d61cbdb', 'Home', '3000') on conflict do nothing;
